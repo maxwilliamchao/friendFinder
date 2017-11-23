0ddd49e8b617ba0f8364e69004ba14a94c04a8e6
maxwilliamchao <maxwilliamchao@gmail.com>
2017-11-22 23:52:00 -0800

    fixed routes

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+
+  {
+    name: 'Mr. Burns',
+    photo: 'http://www.jewornotjew.com/img/people/m/mr__burns.jpg',
+    scores: [
+      '3',
+      '2',
+      '5'
+    ]
+  },
+  {
+    name: 'Lisa',
+    photo: 'https://vignette.wikia.nocookie.net/simpsons/images/e/ed/Lisa-Simpson-icon-1.png/revision/latest?cb=20170628205647',
+    scores: [
+      '1',
+      '5',
+      '1'
+    ]
+  },
+  {
+    name: 'Homer',
+    photo: 'https://vignette.wikia.nocookie.net/mariosonictheedsandfriendsrideoffical/images/a/ab/Homer-simpson.jpg/revision/latest?cb=20140320010101',
+    scores: [
+      '4',
+      '4',
+      '3'
+    ]
+  }
+
+];
+
+module.exports = friends;
